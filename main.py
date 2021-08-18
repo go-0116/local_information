7ccf10cfe108d8c557b68bfbebdb52a9a12f263f
go-0116 <Go.116.matsuda@gmail.com>
2021-08-18 15:32:23 -0700

    binary

--- main.py
+++ main.py
@@ -13,10 +13,10 @@ import streamlit as st
 import base64
 
 chrome_options = Options()
-chrome_options.add_argument("--headless")
+chrome_options.add_argument("--headless", )
 
 def scraping(URL):
-    browser = webdriver.Chrome(options=chrome_options)
+    browser = webdriver.Chrome(executable_path='/home/gm116/bin/chromedriver',options=chrome_options)
     url = URL
     browser.get(url)
 
@@ -106,7 +106,7 @@ selected_url = st.text_input(
 
 def filedownload(df):
     csv = df.to_csv(index=True)
-    b64 = base64.b64encode(csv.encode('shift jis')).decode()  # strings <-> bytes conversions
+    b64 = base64.b64encode(csv.encode()).decode()  # strings <-> bytes conversions
     href = f'<a href="data:file/csv;base64,{b64}" download="ranking.csv">Download CSV File</a>'
     return href
 
